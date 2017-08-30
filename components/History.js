1cc6fd9ce59e9acf6012aa5800b872ac98b5e209
Tyler McGinnis <tylermcginnis33@gmail.com>
2017-08-30 13:59:40 -0600

    Changes for History-AppLoading

--- components/History.js
+++ components/History.js
@@ -8,8 +8,12 @@ import UdaciFitnessCalendar from 'udacifitness-calendar'
 import { white } from '../utils/colors'
 import DateHeader from './DateHeader'
 import MetricCard from './MetricCard'
+import { AppLoading} from 'expo'
 
 class History extends Component {
+  state = {
+    ready: false,
+  }
   componentDidMount () {
     const { dispatch } = this.props
 
@@ -52,6 +56,11 @@ class History extends Component {
   }
   render() {
     const { entries } = this.props
+    const { ready } = this.state
+
+    if (ready === false) {
+      return <AppLoading />
+    }
 
     return (
       <UdaciFitnessCalendar
