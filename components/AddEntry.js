a3e78fe08ac785e6ce372ed37bfb5bcfa19851e3
Tyler McGinnis <tylermcginnis33@gmail.com>
2017-08-30 00:01:45 -0600

    Changes for connect-AddEntry

--- components/AddEntry.js
+++ components/AddEntry.js
@@ -1,12 +1,18 @@
 import React, { Component } from 'react'
 import { View, TouchableOpacity, Text } from 'react-native'
-import { getMetricMetaInfo, timeToString } from '../utils/helpers'
+import {
+  getMetricMetaInfo,
+  timeToString,
+  getDailyReminderValue
+} from '../utils/helpers'
 import UdaciSlider from './UdaciSlider'
 import UdaciSteppers from './UdaciSteppers'
 import DateHeader from './DateHeader'
 import { Ionicons } from '@expo/vector-icons'
 import TextButton from './TextButton'
 import { submitEntry, removeEntry } from '../utils/api'
+import { connect } from 'react-redux'
+import { addEntry } from '../actions'
 
 function SubmitBtn ({ onPress }) {
   return (
@@ -17,7 +23,7 @@ function SubmitBtn ({ onPress }) {
   )
 }
 
-export default class AddEntry extends Component {
+class AddEntry extends Component {
   state = {
     run: 0,
     bike: 0,
@@ -56,7 +62,9 @@ export default class AddEntry extends Component {
     const key = timeToString()
     const entry = this.state
 
-    // Update Redux
+    this.props.dispatch(addEntry({
+      [key]: entry
+    }))
 
     this.setState(() => ({ run: 0, bike: 0, swim: 0, sleep: 0, eat: 0 }))
 
@@ -69,7 +77,9 @@ export default class AddEntry extends Component {
   reset = () => {
     const key = timeToString()
 
-    // Update Redux
+    this.props.dispatch(addEntry({
+      [key]: getDailyReminderValue()
+    }))
 
     // Route to Home
 
@@ -123,3 +133,15 @@ export default class AddEntry extends Component {
     )
   }
 }
+
+function mapStateToProps (state) {
+  const key = timeToString()
+
+  return {
+    alreadyLogged: state[key] && typeof state[key].today === 'undefined'
+  }
+}
+
+export default connect(
+  mapStateToProps
+)(AddEntry)
--- utils/helpers.js
+++ utils/helpers.js
@@ -3,6 +3,12 @@ import { View } from 'react-native'
 import { FontAwesome, MaterialIcons, MaterialCommunityIcons } from '@expo/vector-icons'
 import { white } from './colors'
 
+export function getDailyReminderValue () {
+  return {
+    today: "👋 Don't forget to log your data today!"
+  }
+}
+
 export function getMetricMetaInfo (metric) {
   const info = {
     run: {
