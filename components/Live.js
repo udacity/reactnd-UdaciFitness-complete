62780584135dbfcee999b10ec3f01ec646aeb27b
Tyler McGinnis <tylermcginnis33@gmail.com>
2017-08-30 15:58:13 -0600

    Live-undetermined

--- components/Live.js
+++ components/Live.js
@@ -1,11 +1,16 @@
 import React, { Component } from 'react'
-import { View, Text, ActivityIndicator } from 'react-native'
+import { View, Text, ActivityIndicator, TouchableOpacity, StyleSheet } from 'react-native'
+import { Foundation } from '@expo/vector-icons'
+import { purple, white } from '../utils/colors'
 
 export default class Live extends Component {
   state = {
     coords: null,
-    status: null,
+    status: 'undetermined',
     direction: ''
+  }
+  askPermission = () => {
+
   }
   render() {
     const { status, coords, direction } = this.state
@@ -24,17 +29,50 @@ export default class Live extends Component {
 
     if (status === 'undetermined') {
       return (
-        <View>
-          <Text>undetermined</Text>
+        <View style={styles.center}>
+          <Foundation name='alert' size={50} />
+          <Text>
+            You need to enable location services for this app.
+          </Text>
+          <TouchableOpacity style={styles.button} onPress={this.askPermission}>
+            <Text style={styles.buttonText}>
+              Enable
+            </Text>
+          </TouchableOpacity>
         </View>
       )
     }
 
     return (
-      <View>
+      <View style={styles.container}>
         <Text>Live</Text>
         <Text>{JSON.stringify(this.state)}</Text>
       </View>
     )
   }
 }
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: 'space-between'
+  },
+  center: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
+    marginLeft: 30,
+    marginRight: 30,
+  },
+  button: {
+    padding: 10,
+    backgroundColor: purple,
+    alignSelf: 'center',
+    borderRadius: 5,
+    margin: 20,
+  },
+  buttonText :{
+    color: white,
+    fontSize: 20,
+  }
+})
