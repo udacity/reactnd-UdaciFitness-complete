7d4fba6fba7e0f9833e9645c2f34e9c058c9feed
Tyler McGinnis <tylermcginnis33@gmail.com>
2017-08-30 15:11:56 -0600

    Changes for navigationOptions

--- components/EntryDetail.js
+++ components/EntryDetail.js
@@ -2,6 +2,17 @@ import React, { Component } from 'react'
 import { View, Text } from 'react-native'
 
 class EntryDetail extends Component {
+  static navigationOptions = ({ navigation }) => {
+    const { entryId } = navigation.state.params
+
+    const year = entryId.slice(0, 4)
+    const month = entryId.slice(5,7)
+    const day = entryId.slice(8)
+
+    return {
+      title: `${month}/${day}/${year}`
+    }
+  }
   render() {
     return (
       <View>
