170f06a5b58a7539ae10d483c1407c4697361d34
Tyler McGinnis <tylermcginnis33@gmail.com>
2017-08-29 23:49:09 -0600

    Changes for create-store

--- App.js
+++ App.js
@@ -1,13 +1,18 @@
 import React from 'react'
 import { View } from 'react-native'
 import AddEntry from './components/AddEntry'
+import { createStore } from 'redux'
+import { Provider } from 'react-redux'
+import reducer from './reducers'
 
 export default class App extends React.Component {
   render() {
     return (
-      <View>
-        <AddEntry />
-      </View>
+      <Provider store={createStore(reducer)}>
+        <View>
+          <AddEntry />
+        </View>
+      </Provider>
     )
   }
 }
