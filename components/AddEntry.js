78a78135ab80b78e8585f428052e76573fc8996e
Tyler McGinnis <tylermcginnis33@gmail.com>
2017-08-29 23:31:27 -0600

    Changes for Api

--- components/AddEntry.js
+++ components/AddEntry.js
@@ -6,6 +6,7 @@ import UdaciSteppers from './UdaciSteppers'
 import DateHeader from './DateHeader'
 import { Ionicons } from '@expo/vector-icons'
 import TextButton from './TextButton'
+import { submitEntry, removeEntry } from '../utils/api'
 
 function SubmitBtn ({ onPress }) {
   return (
@@ -61,7 +62,7 @@ export default class AddEntry extends Component {
 
     // Navigate to home
 
-    // Save to "DB"
+    submitEntry({ key, entry })
 
     // Clear local notification
   }
@@ -72,7 +73,7 @@ export default class AddEntry extends Component {
 
     // Route to Home
 
-    // Update "DB"
+    removeEntry(key)
   }
   render() {
     const metaInfo = getMetricMetaInfo()
---
+++ utils/api.js
@@ -0,0 +1,18 @@
+import { AsyncStorage } from 'react-native'
+import { CALENDAR_STORAGE_KEY } from './_calendar'
+
+export function submitEntry ({ entry, key }) {
+  return AsyncStorage.mergeItem(CALENDAR_STORAGE_KEY, JSON.stringify({
+    [key]: entry
+  }))
+}
+
+export function removeEntry (key) {
+  return AsyncStorage.getItem(CALENDAR_STORAGE_KEY)
+    .then((results) => {
+      const data = JSON.parse(results)
+      data[key] = undefined
+      delete data[key]
+      AsyncStorage.setItem(CALENDAR_STORAGE_KEY, JSON.stringify(data))
+    })
+}
