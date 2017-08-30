3aa3f69d21b8a96b4b9d99f67b655772a479095f
Tyler McGinnis <tylermcginnis33@gmail.com>
2017-08-29 23:01:10 -0600

    Changes for AddEntry-render

--- components/AddEntry.js
+++ components/AddEntry.js
@@ -1,6 +1,8 @@
 import React, { Component } from 'react'
 import { View } from 'react-native'
 import { getMetricMetaInfo } from '../utils/helpers'
+import UdaciSlider from './UdaciSlider'
+import UdaciSteppers from './UdaciSteppers'
 
 export default class AddEntry extends Component {
   state = {
@@ -38,9 +40,32 @@ export default class AddEntry extends Component {
     }))
   }
   render() {
+    const metaInfo = getMetricMetaInfo()
+
     return (
       <View>
+        {Object.keys(metaInfo).map((key) => {
+          const { getIcon, type, ...rest } = metaInfo[key]
+          const value = this.state[key]
 
+          return (
+            <View key={key}>
+              {getIcon()}
+              {type === 'slider'
+                ? <UdaciSlider
+                    value={value}
+                    onChange={(value) => this.slide(key, value)}
+                    {...rest}
+                  />
+                : <UdaciSteppers
+                    value={value}
+                    onIncrement={() => this.increment(key)}
+                    onDecrement={() => this.decrement(key)}
+                    {...rest}
+                  />}
+            </View>
+          )
+        })}
       </View>
     )
   }
---
+++ components/UdaciSlider.js
@@ -0,0 +1,10 @@
+import React from 'react'
+import { View, Text } from 'react-native'
+
+export default function UdaciSlider () {
+  return (
+    <View>
+      <Text>UdaciSlider</Text>
+    </View>
+  )
+}
