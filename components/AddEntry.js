18aeee6aac40702c2d86cf976a9a67c5691505cf
Tyler McGinnis <tylermcginnis33@gmail.com>
2017-08-30 15:23:03 -0600

    Changes for finish-navigation

--- components/AddEntry.js
+++ components/AddEntry.js
@@ -14,6 +14,7 @@ import { submitEntry, removeEntry } from '../utils/api'
 import { connect } from 'react-redux'
 import { addEntry } from '../actions'
 import { purple, white } from '../utils/colors'
+import { NavigationActions } from 'react-navigation'
 
 function SubmitBtn ({ onPress }) {
   return (
@@ -69,7 +70,7 @@ class AddEntry extends Component {
 
     this.setState(() => ({ run: 0, bike: 0, swim: 0, sleep: 0, eat: 0 }))
 
-    // Navigate to home
+    this.toHome()
 
     submitEntry({ key, entry })
 
@@ -82,9 +83,12 @@ class AddEntry extends Component {
       [key]: getDailyReminderValue()
     }))
 
-    // Route to Home
+    this.toHome()
 
     removeEntry(key)
+  }
+  toHome = () => {
+    this.props.navigation.dispatch(NavigationActions.back({key: 'AddEntry'}))
   }
   render() {
     const metaInfo = getMetricMetaInfo()
