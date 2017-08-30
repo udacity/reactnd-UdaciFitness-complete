556a167f9f773c128c6d982c27130fe7b0b27d82
Tyler McGinnis <tylermcginnis33@gmail.com>
2017-08-30 13:56:40 -0600

    Changes for History-methods

--- components/History.js
+++ components/History.js
@@ -1,10 +1,12 @@
 import React, { Component } from 'react'
-import { View, Text } from 'react-native'
+import { View, Text, StyleSheet, Platform, TouchableOpacity } from 'react-native'
 import { connect } from 'react-redux'
 import { receiveEntries, addEntry } from '../actions'
 import { timeToString, getDailyReminderValue } from '../utils/helpers'
 import { fetchCalendarResults } from '../utils/api'
 import UdaciFitnessCalendar from 'udacifitness-calendar'
+import { white } from '../utils/colors'
+import DateHeader from './DateHeader'
 
 class History extends Component {
   componentDidMount () {
@@ -22,16 +24,28 @@ class History extends Component {
       .then(() => this.setState(() => ({ready: true})))
   }
   renderItem = ({ today, ...metrics }, formattedDate, key) => (
-    <View>
+    <View style={styles.item}>
       {today
-        ? <Text>{JSON.stringify(today)}</Text>
-        : <Text>{JSON.stringify(metrics)}</Text>}
+        ? <View>
+            <DateHeader date={formattedDate}/>
+            <Text style={styles.noDataText}>
+              {today}
+            </Text>
+          </View>
+        : <TouchableOpacity
+            onPress={() => console.log('Pressed!')}
+          >
+              <Text>{JSON.stringify(metrics)}</Text>
+          </TouchableOpacity>}
     </View>
   )
   renderEmptyDate(formattedDate) {
     return (
-      <View>
-        <Text>No Data for this day</Text>
+      <View style={styles.item}>
+        <DateHeader date={formattedDate}/>
+        <Text style={styles.noDataText}>
+          You didn't log any data on this day.
+        </Text>
       </View>
     )
   }
@@ -47,6 +61,31 @@ class History extends Component {
     )
   }
 }
+
+const styles = StyleSheet.create({
+  item: {
+    backgroundColor: white,
+    borderRadius: Platform.OS === 'ios' ? 16 : 2,
+    padding: 20,
+    marginLeft: 10,
+    marginRight: 10,
+    marginTop: 17,
+    justifyContent: 'center',
+    shadowRadius: 3,
+    shadowOpacity: 0.8,
+    shadowColor: 'rgba(0, 0, 0, 0.24)',
+    shadowOffset: {
+      width: 0,
+      height: 3
+    },
+  },
+  noDataText: {
+    fontSize: 20,
+    paddingTop: 20,
+    paddingBottom: 20
+  }
+})
+
 
 function mapStateToProps (entries) {
   return {
