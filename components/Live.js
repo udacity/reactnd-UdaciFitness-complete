70aa708e381b86471da886e01efc6568df076c54
Tyler McGinnis <tylermcginnis33@gmail.com>
2017-08-30 15:59:11 -0600

    Changes for Live-denied

--- components/Live.js
+++ components/Live.js
@@ -6,7 +6,7 @@ import { purple, white } from '../utils/colors'
 export default class Live extends Component {
   state = {
     coords: null,
-    status: 'undetermined',
+    status: 'denied',
     direction: ''
   }
   askPermission = () => {
@@ -21,8 +21,11 @@ export default class Live extends Component {
 
     if (status === 'denied') {
       return (
-        <View>
-          <Text>Denied</Text>
+        <View style={styles.center}>
+          <Foundation name='alert' size={50} />
+          <Text>
+            You denied your location. You can fix this by visiting your settings and enabling location services for this app.
+          </Text>
         </View>
       )
     }
