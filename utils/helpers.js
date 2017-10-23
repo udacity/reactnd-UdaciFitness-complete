42d07778455d746adf077b90cf1356230be2e2ef
Andrew Wong <hbkwong@ucdavis.edu>
2017-10-23 14:49:55 -0700

    Update helpers.js.

--- utils/helpers.js
+++ utils/helpers.js
@@ -200,9 +200,9 @@ export function setLocalNotification () {
               let tomorrow = new Date()
               tomorrow.setDate(tomorrow.getDate() + 1)
               tomorrow.setHours(20)
-              tomorrow.setMintutes(0)
+              tomorrow.setMinutes(0)
 
-              Notifications.scheduleLocalNotificationsAsync(
+              Notifications.scheduleLocalNotificationAsync(
                 createNotification(),
                 {
                   time: tomorrow,
