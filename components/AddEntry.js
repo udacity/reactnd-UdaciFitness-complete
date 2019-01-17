c3b244a93a6fdd484cfd9306fc05e0c2095bdbe4
Andrew Wong <hbkwong@ucdavis.edu>
2019-01-17 15:26:16 -0800

    Update AddEntry and helpers.

--- components/AddEntry.js
+++ components/AddEntry.js
@@ -1,13 +1,9 @@
-import React, { Component } from 'react'
-import { View } from 'react-native'
-import { getMetricMetaInfo } from '../utils/helpers'
+import React, { Component } from "react";
+import { View } from "react-native";
+import { getMetricMetaInfo } from "../utils/helpers";
 
 export default class AddEntry extends Component {
   render() {
-    return (
-      <View>
-
-      </View>
-    )
+    return <View>{getMetricMetaInfo("bike").getIcon}</View>;
   }
 }
--- utils/helpers.js
+++ utils/helpers.js
@@ -1,145 +1,129 @@
-import React from 'react'
-import { View } from 'react-native'
-import { FontAwesome, MaterialIcons, MaterialCommunityIcons } from '@expo/vector-icons'
-import { white } from './colors'
+import React from "react";
+import { View } from "react-native";
+import {
+  FontAwesome,
+  MaterialIcons,
+  MaterialCommunityIcons
+} from "@expo/vector-icons";
+import { white } from "./colors";
 
-export function getMetricMetaInfo (metric) {
+export function getMetricMetaInfo(metric) {
   const info = {
     run: {
-      displayName: 'Run',
+      displayName: "Run",
       max: 50,
-      unit: 'miles',
+      unit: "miles",
       step: 1,
-      type: 'steppers',
+      type: "steppers",
       getIcon() {
         return (
           <View>
-            <MaterialIcons
-              name='directions-run'
-              color={white}
-              size={35}
-            />
+            <MaterialIcons name="directions-run" color={"black"} size={35} />
           </View>
-        )
+        );
       }
     },
     bike: {
-      displayName: 'Bike',
+      displayName: "Bike",
       max: 100,
-      unit: 'miles',
+      unit: "miles",
       step: 1,
-      type: 'steppers',
+      type: "steppers",
       getIcon() {
         return (
           <View>
-            <MaterialCommunityIcons
-              name='bike'
-              color={white}
-              size={32}
-            />
+            <MaterialCommunityIcons name="bike" color={white} size={32} />
           </View>
-        )
+        );
       }
     },
     swim: {
-      displayName: 'Swim',
+      displayName: "Swim",
       max: 9900,
-      unit: 'meters',
+      unit: "meters",
       step: 100,
-      type: 'steppers',
+      type: "steppers",
       getIcon() {
         return (
           <View>
-            <MaterialCommunityIcons
-              name='swim'
-              color={white}
-              size={35}
-            />
+            <MaterialCommunityIcons name="swim" color={white} size={35} />
           </View>
-        )
+        );
       }
     },
     sleep: {
-      displayName: 'Sleep',
+      displayName: "Sleep",
       max: 24,
-      unit: 'hours',
+      unit: "hours",
       step: 1,
-      type: 'slider',
+      type: "slider",
       getIcon() {
         return (
           <View>
-            <FontAwesome
-              name='bed'
-              color={white}
-              size={30}
-            />
+            <FontAwesome name="bed" color={white} size={30} />
           </View>
-        )
+        );
       }
     },
     eat: {
-      displayName: 'Eat',
+      displayName: "Eat",
       max: 10,
-      unit: 'rating',
+      unit: "rating",
       step: 1,
-      type: 'slider',
+      type: "slider",
       getIcon() {
         return (
           <View>
-            <MaterialCommunityIcons
-              name='food'
-              color={white}
-              size={35}
-            />
+            <MaterialCommunityIcons name="food" color={white} size={35} />
           </View>
-        )
+        );
       }
-    },
-  }
+    }
+  };
 
-  return typeof metric === 'undefined'
-    ? info
-    : info[metric]
+  return typeof metric === "undefined" ? info : info[metric];
 }
 
-export function isBetween (num, x, y) {
+export function isBetween(num, x, y) {
   if (num >= x && num <= y) {
-    return true
+    return true;
   }
 
-  return false
+  return false;
 }
 
-export function calculateDirection (heading) {
-  let direction = ''
+export function calculateDirection(heading) {
+  let direction = "";
 
   if (isBetween(heading, 0, 22.5)) {
-    direction = 'North'
+    direction = "North";
   } else if (isBetween(heading, 22.5, 67.5)) {
-    direction = 'North East'
+    direction = "North East";
   } else if (isBetween(heading, 67.5, 112.5)) {
-    direction = 'East'
+    direction = "East";
   } else if (isBetween(heading, 112.5, 157.5)) {
-    direction = 'South East'
+    direction = "South East";
   } else if (isBetween(heading, 157.5, 202.5)) {
-    direction = 'South'
+    direction = "South";
   } else if (isBetween(heading, 202.5, 247.5)) {
-    direction = 'South West'
+    direction = "South West";
   } else if (isBetween(heading, 247.5, 292.5)) {
-    direction = 'West'
+    direction = "West";
   } else if (isBetween(heading, 292.5, 337.5)) {
-    direction = 'North West'
+    direction = "North West";
   } else if (isBetween(heading, 337.5, 360)) {
-    direction = 'North'
+    direction = "North";
   } else {
-    direction = 'Calculating'
+    direction = "Calculating";
   }
 
-  return direction
+  return direction;
 }
 
-export function timeToString (time = Date.now()) {
-  const date = new Date(time)
-  const todayUTC = new Date(Date.UTC(date.getFullYear(), date.getMonth(), date.getDate()))
-  return todayUTC.toISOString().split('T')[0]
+export function timeToString(time = Date.now()) {
+  const date = new Date(time);
+  const todayUTC = new Date(
+    Date.UTC(date.getFullYear(), date.getMonth(), date.getDate())
+  );
+  return todayUTC.toISOString().split("T")[0];
 }
