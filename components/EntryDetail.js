5d77f8d831e170fc0ffdeae1bc92a0825e71e14a
Andrew Wong <hbkwong@ucdavis.edu>
2019-01-17 16:13:37 -0800

    Update module import in EntryDetail.

--- components/EntryDetail.js
+++ components/EntryDetail.js
@@ -1,31 +1,34 @@
-import React, { Component } from 'react'
-import { View, Text, StyleSheet } from 'react-native'
-import { connect } from 'react-redux'
-import { timeToString, getDailyReminderValue } from '../utils/helpers'
-import MetricCard from './MetricCard'
-import { white } from '../utils/helpers'
+import React, { Component } from "react";
+import { View, Text, StyleSheet } from "react-native";
+import { connect } from "react-redux";
+import { timeToString, getDailyReminderValue } from "../utils/helpers";
+import MetricCard from "./MetricCard";
+import { white } from "../utils/colors";
 
 class EntryDetail extends Component {
   static navigationOptions = ({ navigation }) => {
-    const { entryId } = navigation.state.params
+    const { entryId } = navigation.state.params;
 
-    const year = entryId.slice(0, 4)
-    const month = entryId.slice(5,7)
-    const day = entryId.slice(8)
+    const year = entryId.slice(0, 4);
+    const month = entryId.slice(5, 7);
+    const day = entryId.slice(8);
 
     return {
       title: `${month}/${day}/${year}`
-    }
-  }
+    };
+  };
   render() {
-    const { metrics } = this.props
+    const { metrics } = this.props;
 
     return (
       <View style={styles.container}>
         <MetricCard metrics={metrics} />
-        <Text>Entry Detail - {JSON.stringify(this.props.navigation.state.params.entryId)}</Text>
+        <Text>
+          Entry Detail -{" "}
+          {JSON.stringify(this.props.navigation.state.params.entryId)}
+        </Text>
       </View>
-    )
+    );
   }
 }
 
@@ -33,19 +36,17 @@ const styles = StyleSheet.create({
   container: {
     flex: 1,
     backgroundColor: white,
-    padding: 15,
-  },
-})
+    padding: 15
+  }
+});
 
-function mapStateToProps (state, { navigation }) {
-  const { entryId } = navigation.state.params
+function mapStateToProps(state, { navigation }) {
+  const { entryId } = navigation.state.params;
 
   return {
     entryId,
-    metrics: state[entryId],
-  }
+    metrics: state[entryId]
+  };
 }
 
-export default connect(
-  mapStateToProps,
-)(EntryDetail)
+export default connect(mapStateToProps)(EntryDetail);
